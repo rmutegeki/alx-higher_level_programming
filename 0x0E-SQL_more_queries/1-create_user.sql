35b89ad91c72a1d123a393e13687f4c840582a7e
Ronald Mutegeki <murogive@gmail.com>
2022-12-01 11:43:30 +0900

    Add: 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script that creates an user in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
