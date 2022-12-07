d1c19cba6de49d9fcb93bd0e9efa600b58f36e81
Ronald Mutegeki <murogive@gmail.com>
2022-12-07 22:21:33 +0900

    Update: Corrections

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -30,7 +30,7 @@ class Magic:
         """ print status code """
         print("File size: {:d}".format(self.size))
         for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
+            if self.dic[key] != 0:
                 print("{}: {:d}".format(key, self.dic[key]))
 
 
@@ -41,7 +41,7 @@ if __name__ == "__main__":
 
     try:
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
+            if nlines % 10 == 0 and nlines != 0:
                 magic.print_info()
 
             try:
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,7 +5,7 @@ data structure for a JSON serialization of an object
 
 
 def class_to_json(obj):
-    """ Function that retuns the dictionary description of an obj """
+    """ Function that returns the dictionary description of an obj """
 
     res = {}
     if hasattr(obj, "__dict__"):
