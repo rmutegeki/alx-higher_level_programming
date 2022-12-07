35a3b96530b1b2255fa9d609ec94f865a0527ee0
Ronald Mutegeki <murogive@gmail.com>
2022-12-07 22:41:30 +0900

    Update: Corrections

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-Module 9-add_item
+Module 7-add_item
 Contains function that adds and saves to Python obj to JSON file; loads objects
-# run with ./9-add_item.py
+# run with ./7-add_item.py
 #
 # cat add_item.json ; echo ""
 # expect output: []
 #
-# ./9-add_item.py some random args
+# ./7-add_item.py some random args
 # cat add_item.json ; echo ""
 # expect output: ["some", "random", "args"]
 """
 
 
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
