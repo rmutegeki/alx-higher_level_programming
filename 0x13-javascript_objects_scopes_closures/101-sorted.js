637bf5c1e24f1c17f7dc02eb499a3bbb924f8326
Ronald Mutegeki <murogive@gmail.com>
2022-12-20 18:59:36 -0800

    Correction

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-let newDic = {};
-for (let key in dict) {
+const newDic = {};
+for (const key in dict) {
   if (newDic[dict[key]] === undefined) {
     newDic[dict[key]] = [];
   }
