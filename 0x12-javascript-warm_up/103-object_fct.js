216e73397df64c228aa44e09b97800deb05b3349
Ronald Mutegeki <murogive@gmail.com>
2022-12-20 18:26:57 -0800

    Added 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,13 +4,10 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
-myObject.incr = (function () { this.value += 1 });
-
+function incr() { myObject.value += 1;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
