74d715d3f50cd5e353fa14abfe713a3f8a9faf17
Ronald Mutegeki <murogive@gmail.com>
2023-06-13 09:53:43 +0900

    Update 103-object_fct.js
    Updated

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,11 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-function incr() { myObject.value += 1;
+
+myObject.incr = function() {
+  this.value++;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
